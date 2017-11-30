36b1611874545e8f5182628a9a8054c2309d67fa
giwiro <giwirodavalos@gmail.com>
2017-11-30 00:10:02 -0500

    get mongo uri as env variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from app.db import mongo, init_db
 from app.splash import splash_module
@@ -11,7 +12,11 @@ def set_up_routes(app):
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile("config.py")
+    # app.config.from_pyfile("config.py")
+
+    mongo_uri = os.environ.get('MONGO_URI') or "mongodb://localhost/cbf"
+    app.config["MONGO_URI"] = mongo_uri
+
     init_db(app)
     set_up_routes(app)
     return app
