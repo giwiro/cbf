6a042615dda2b4f68c9a866d7c89cb64d06610f6
giwiro <giwirodavalos@gmail.com>
2017-11-30 00:23:58 -0500

    fix correct index file to server at /

--- app/splash/__init__.py
+++ app/splash/__init__.py
@@ -6,6 +6,6 @@ splash_module = Blueprint("splash", __name__)
 @splash_module.route("/")
 def show_splash():
     books = mongo.db.books.find()
-    return render_template("index.html", books=books)
+    return render_template("index2.html", books=books)
 
 
