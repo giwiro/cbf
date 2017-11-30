538beda799b2858db85d44b7e149d1c661c3d088
Renzo <rburgape@gmail.com>
2017-11-30 00:30:43 -0500

    fix similarities

--- app/search/__init__.py
+++ app/search/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request
 from app.db import mongo
 
-import pprint
 search_module = Blueprint("search", __name__)
 
 @search_module.route("/search", methods=["POST"])
@@ -19,13 +18,13 @@ def search():
 
     # pprint.pprint(books_value)
     result_query = {"id": {"$in": book_ids}}
-    books_result = mongo.db.books.find(result_query).sort([("value", -1)]).limit(10)
+    books_result = mongo.db.books.find(result_query).sort([("value", -1)])
     for br in books_result:
         books_value[int(br["id"])].update(br)
-    
-
+    books_nepe = list(books_value.values())[:10]
+    books_nepe.sort(key=lambda x:x['value'],reverse=True)
     # pprint.pprint(books_value)
     request_book = mongo.db.books.find_one({"id": id})
     return render_template("result.html",
-            request_book=request_book, book_ids=book_ids, books=list(books_value.values()),)
+            request_book=request_book, book_ids=book_ids, books=books_nepe,)
 
