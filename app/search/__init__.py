597c68876e009b8c4dc2f6b628c62b688d2a0468
giwiro <giwirodavalos@gmail.com>
2017-11-29 19:55:10 -0500

    ?

--- app/search/__init__.py
+++ app/search/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 from app.db import mongo
 
+import pprint
 search_module = Blueprint("search", __name__)
 
 @search_module.route("/search", methods=["POST"])
@@ -8,10 +9,23 @@ def search():
     id = int(request.form["book_id"])
     query = {"$or": [{ "bookA": id }, { "boolB": id }]}
     result = mongo.db.relations.find(query)
-    book_ids = [int(r["bookA"]) if r["bookA"] != id else r["bookB"] for r in result]
+    book_ids = []
+    books_value = {}
+    for r in result:
+        key = int(r["bookA"]) if r["bookA"] != id else int(r["bookB"])
+        book_ids.append(key)
+        content = dict({"value": r["value"]})
+        books_value[key] = content
+
+    # pprint.pprint(books_value)
     result_query = {"id": {"$in": book_ids}}
-    books = mongo.db.books.find(result_query).sort([("value", -1)]).limit(10)
+    books_result = mongo.db.books.find(result_query).sort([("value", -1)]).limit(10)
+    for br in books_result:
+        books_value[int(br["id"])].update(br)
+    
+
+    # pprint.pprint(books_value)
     request_book = mongo.db.books.find_one({"id": id})
     return render_template("result.html",
-            request_book=request_book, book_ids=book_ids, books=books,)
+            request_book=request_book, book_ids=book_ids, books=list(books_value.values()),)
 
